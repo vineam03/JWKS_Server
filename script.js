d7a00b5cfbb87d4837c399b0b95f8c3f2ecd8406
vineam03 <66529553+vineam03@users.noreply.github.com>
2024-02-07 16:57:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const express = require('express');
 //An instantiation of express, assigned to object app. This is a common practice when using express
 const app = new express();
 //Default to port 5000, we will use this variable later
-const port = 5000
+const port = 8080
 
 app.listen(port, () => {
   console.log('Listening on port number ${port}');
