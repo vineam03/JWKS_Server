fd877e755d9a48e5de77e964acb07174d8780f27
vineam03 <66529553+vineam03@users.noreply.github.com>
2024-02-07 16:40:56 -0600

    Update script.js with listening on port 5000

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//This line starts off by incorporating express
+const express = require('express');
+//An instantiation of express, assigned to object app. This is a common practice when using express
+const app = new express();
+//Default to port 5000, we will use this variable later
+const port = 5000
+
+app.listen(port, () => {
+  console.log('Listening on port number ${port}');
+});
+
+
 
